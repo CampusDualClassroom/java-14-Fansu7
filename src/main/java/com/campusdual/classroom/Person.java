8c232dbaf0ce3a4e732ae7731aa6440ed9db03e8
Alfonso Vidal <vidaltorres.alfonso@gmail.com>
2024-11-14 19:35:05 +0100

    Comentar setter de Person porque nunca se utilizan para este ejercicio.

--- src/main/java/com/campusdual/classroom/Person.java
+++ src/main/java/com/campusdual/classroom/Person.java
@@ -13,17 +13,17 @@ public class Person {
 		return this.name;
 	}
 
-	public void setName(String name) {
-		this.name = name;
-	}
+//	public void setName(String name) {
+//		this.name = name;
+//	}
 
 	public String getSurname() {
 		return this.surname;
 	}
 
-	public void setSurname(String surname) {
-		this.surname = surname;
-	}
+//	public void setSurname(String surname) {
+//		this.surname = surname;
+//	}
 
 	public void getDetails(){
 		StringBuilder builder = new StringBuilder();
